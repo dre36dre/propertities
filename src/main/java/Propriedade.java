46dc8b5ecbde3aa9df6bbb8a1d071a9f93d2024a
Anderson freires <dre.son36@gmail.com>
2023-09-04 20:25:55 -0300

    getters and setters

--- src/main/java/Propriedade.java
+++ src/main/java/Propriedade.java
@@ -10,5 +10,37 @@ public class Propriedade {
 	private String descricao;
 	private String categoria;
 	private String subcateria;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getSubcateria() {
+		return subcateria;
+	}
+	public void setSubcateria(String subcateria) {
+		this.subcateria = subcateria;
+	}
 
+	
+	
 }
